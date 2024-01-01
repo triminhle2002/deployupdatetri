967fe9eace060e84a66934f2655986a69c79d2d2
triminhle2002 <97251680+triminhle2002@users.noreply.github.com>
2024-01-02 02:08:28 +0700

    update chatbox

--- src/components/Chatbot/Chatbot.jsx
+++ src/components/Chatbot/Chatbot.jsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 const Chatbot = (props) => {
   useEffect(() => {
     var chatbox = document.getElementById("fb-customer-chat");
-    chatbox.setAttribute("page_id", "227739294418179");
+    chatbox.setAttribute("page_id", "2078121735572443");
     chatbox.setAttribute("attribution", "biz_inbox");
 
     window.fbAsyncInit = function () {
